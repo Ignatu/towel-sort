61b5b39108a55684eb4055db4bbe6b055df1f8ce
Ignatu <48059391+Ignatu@users.noreply.github.com>
2020-10-25 19:10:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (array) {
-  if (array === []){return []} else {
   let arrayJ = array.join(',').split(',');
   for (i=0; i<arrayJ.length; i++) {
     arrayJ[i] = Number(arrayJ[i])}
     return arrayJ.sort()
-  }
 }
 
