315e592c79a1e29292223432ff7d0c3f6bae71c9
Ignatu <48059391+Ignatu@users.noreply.github.com>
2020-10-25 19:03:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (array) {
+      if (array === []){return []} else {
+  let arrayJ = array.join(',').split(',');
+  for (i = 0; i < arrayJ.length; i++) {
+    arrayJ[i] = Number(arrayJ[i])}
+    return arrayJ.sort()}
+  }
 }
